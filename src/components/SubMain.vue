74742dc5043928b44902b29b0b575ff072a3896e
christopherzito <christopherzito11@gmail.com>
2021-11-25 15:57:08 +0100

    update jumbo & comics

--- src/components/SubMain.vue
+++ src/components/SubMain.vue
@@ -1,13 +1,49 @@
 <template>
-  <div>
-      Ciao
+  <div class="box">
+      <div class="comics">
+        <img :src="dcBook.thumb" :alt="dcBook.series">
+      </div>
+      <h3>
+        {{dcBook.series}}
+      </h3>
+      <span>
+        {{dcBook.type}}
+        <br>
+        {{dcBook.price}}
+      </span>
   </div>
 </template>
 
 <script>
 export default {
   name: 'SubMain',
+  props: {
+    dcBook: Object,
+  }
 }
 </script>
 <style scoped lang="scss">
+.box{
+  width: calc((100% / 6) - 20px);
+  margin: 10px;
+  /* debug */
+  /* height: 100px; */
+  /* background-color: red; */
+  /* border: 1px solid white; */
+
+  .comics {
+    width: 100%;
+    /* debug */
+    /* height: 80%;
+    background-color: black; */
+
+    img{
+      width: 100%;
+    }
+  }
+
+  h3 {
+    color: white;
+  }
+}
 </style>
